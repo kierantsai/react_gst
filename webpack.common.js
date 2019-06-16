76732e1c055d1fc99d8a952c97d485882a7b87f4
Kieran Tsai <kierantsai@gmail.com>
2019-06-16 21:42:31 +1200

    Added app-manifest and app icon.

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 const WorkboxPlugin = require('workbox-webpack-plugin');
 var webpack = require('webpack');
 
@@ -32,8 +33,19 @@ module.exports = {
         new HtmlWebpackPlugin({
             title: 'GST Calculator',
             template: './app/index.html',
-            favicon: './public/favicon.ico'
+            favicon: './public/favicon.ico',
+            inject: true
         }),
+        new CopyPlugin([
+            {
+                from: './public/manifest.json',
+                to: './manifest.json'
+            },
+            {
+                from: './public/gst.png',
+                to: './gst.png'
+            }
+        ]),
         new webpack.HotModuleReplacementPlugin(),
         new webpack.ProvidePlugin({
             $: 'jquery',
